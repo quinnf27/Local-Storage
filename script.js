c7dfab30f5e8566f390b120c9865ccf4d4a741c9
quinnf27 <quinnferguson@students.berkeley.net>
2026-02-24 19:38:21 +0000

    update

--- script.js
+++ script.js
@@ -1,6 +1,119 @@
-document.getElementById("button").addEventListener("click",myFunc);
+// document.getElementById("button").addEventListener("click",myFunc);
 
-function myFunc(){
-    document.getElementById("output").innerHTML = "hi friend!";
-}
+// function myFunc(){
+//     document.getElementById("output").innerHTML = "hi friend!";
+// }
+
+class Exercise 
+    constructor(time, intensity, bodypart, materials, location, calories){
+        this.time = time;
+        this.intensity = intensity;
+        this.bodypart = bodypart;
+        this.materials = materials;
+        this.location = location;
+        this.calories = calories;
+    }
+
+    //functions?
+    let squats = {
+        time = "10 minutes";
+        intensity = "medium";
+        bodypart = "";
+        materials ="";
+        location = "Inside";
+        calories = "";
+    }
+
+    let run = {
+        time = "30 minutes";
+        intensity = "high";
+        bodypart = "legs";
+        materials = "";
+        location = "Outside";
+        calories = "";
+    }
+
+    let lunges = {
+        time = "10 minutes";
+        intensity = "medium";
+        bodypart = "legs";
+        materials = "";
+        location = "Inside";
+        calories = "";
+    }
+
+    let calf raises = {
+        time = "5 minutes";
+        intensity = "low";
+        bodypart = "legs";
+        materials ="";
+        location = "Inside";
+        calories = "";
+    }
+
+    let jumpsquats = {
+        time = "10 minutes";
+        intensity = "high";
+        bodypart = "legs";
+        materials ="";
+        location = "Inside";
+        calories = "";
+    }
+
+    let jumping jacks = {
+        time = "10 minutes";
+        intensity = "low";
+        bodypart = "core";
+        materials ="";
+        location = "Inside";
+        calories = "";
+    }
+
+    let hike = {
+        time = "1 hour";
+        intensity = "low";
+        bodypart = "legs";
+        materials ="";
+        location = "Outside";
+        calories = "";
+    }
+
+    let  = {
+        time = "10 minutes";
+        intensity = "";
+        bodypart = "";
+        materials ="";
+        location = "Inside";
+        calories = "";
+    }
+
+    let  = {
+        time = "10 minutes";
+        intensity = "";
+        bodypart = "";
+        materials ="";
+        location = "Inside";
+        calories = "";
+    }
+
+    let  = {
+        time = "10 minutes";
+        intensity = "";
+        bodypart = "";
+        materials ="";
+        location = "Inside";
+        calories = "";
+    }
+
+
+/*
+
+1. make your exercise class with the properties in your wireframe
+2. create some exercises using the class
+3. create the html input form, like your wireframe
+4. build the output table that shows on a search
+
+Can the user enter a search and get the right exercises displayed on the page?
+
+*/
 
